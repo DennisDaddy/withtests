6576c47c2865385ed6b66e110078069682c24a3b
DennisDaddy <softteez95@gmail.com>
2016-12-14 09:54:50 +0300

    add setup method

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    
+  end
+
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
