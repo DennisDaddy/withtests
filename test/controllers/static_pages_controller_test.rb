5410510048087e694d196ea8ad28c9e5ceb677b3
DennisDaddy <softteez95@gmail.com>
2016-12-14 10:30:52 +0300

    remove all errors

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,7 +10,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails"
     
   end
 
