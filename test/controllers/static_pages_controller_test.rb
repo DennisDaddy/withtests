93e546487f9d087b367771c7e554cb4def5b6307
DennisDaddy <softteez95@gmail.com>
2016-12-15 08:10:46 +0300

    test site layout using intergration  test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -26,10 +26,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | Ruby on Rails"
     end
 
-    test "Should get contact" do
-    get contact_url
-    assert_response :success
-    assert_select "title", "Contact | Ruby on Rails"
-    end
-
 end
