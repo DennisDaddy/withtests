1d8ae547e20aa9fa24df6f5332419fbf17d65c28
DennisDaddy <softteez95@gmail.com>
2016-12-16 08:40:28 +0300

    case-insensitive email uniqueness.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -43,6 +43,7 @@ test "email should not be too long" do
 
  test "email addresses should be unique" do
 		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
 		@user.save
 		assert_not duplicate_user.valid?	
   end
