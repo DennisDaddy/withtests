dd193ff86e24605155a15a0236552031cb5e55ae
DennisDaddy <softteez95@gmail.com>
2016-12-16 13:13:45 +0300

    Adding a password and its confirmation.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
  def setup
-   @user = User.new(name: "Dennis Kings", email: "dennis@gmail.com")
+   @user = User.new(name: "Dennis Kings", email: "dennis@gmail.com",
+   	password: "foobar", password_confirmation: "foobar")
 end
 
 test "should be valid" do
