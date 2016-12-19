7e6acfb134d3361072bb61f683974dcc1711e692
DennisDaddy <softteez95@gmail.com>
2016-12-19 08:40:10 +0300

    remove route error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'	
   
   root 'static_pages#home'
 
@@ -13,7 +14,7 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
 
   post 'login' => 'sessions#create'
-  
+
   delete 'logout' => 'sessions#destroy'
 
   resources :users
