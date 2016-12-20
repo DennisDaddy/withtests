1dfdd9f009bb2360976b3d7237a4d52559c6800c
DennisDaddy <softteez95@gmail.com>
2016-12-20 19:51:42 +0300

    remove repeated test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,12 +10,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
   end
-
-  test "should get new" do
-	get :new
-	assert_response :success
-	end
-
+  
 	test "should redirect edit when not logged in" do
 		get :edit, id: @user
 		assert_not flash.empty?
@@ -34,7 +29,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 		assert flash.empty?
 		assert_redirected_to root_url
 	end
-	
+
 	test "should redirect update when logged in as wrong user" do
 		log_in_as(@other_user)
 		patch :update, id: @user, user: { name: @user.name, email: @user.email }
