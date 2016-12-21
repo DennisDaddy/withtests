d7c6b59c852a2b30dd609c767a5d1f83954b7e78
DennisDaddy <softteez95@gmail.com>
2016-12-21 17:26:16 +0300

    destroy mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,8 +18,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'config.action_mailer.raise_delivery_errors = true
-c
+      'Cache-Control' => 'public, max-age=172800'
     }
   else
     config.action_controller.perform_caching = false
@@ -28,12 +27,7 @@ c
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
-  config.action_mailer.defauconfig.action_mailer.raise_delivery_errors = true
-clt_url_options = { host: host }
-
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
@@ -44,13 +38,12 @@ clt_url_options = { host: host }
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause siconfig.action_mailer.raise_delivery_errors = true
-cgnificant delays in view rendering with a large
+  # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = trueconfig.acconfig.action_mailer.raise_delivery_errors = true
-ction_mailer.raise_delivery_errors = true
-cconfig.action_mailer.raise_delivery_errors = true
-c
+  config.assets.debug = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
