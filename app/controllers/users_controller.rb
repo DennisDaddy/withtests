577b291782dffc67d4e39b5f4662809a60a45057
DennisDaddy <softteez95@gmail.com>
2016-12-21 12:25:36 +0300

    detroy admin migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
 	 before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 	 before_action :correct_user, only: [:edit, :update]
+	 before_action :admin_user, only: :destroy
 
   def show
   	@user =User.find(params[:id])
@@ -72,6 +73,11 @@ class UsersController < ApplicationController
 	def correct_user
 	@user = User.find(params[:id])
 	redirect_to(root_url) unless @user == current_user
+	end
+
+	# Confirms an admin user.
+	def admin_user
+	redirect_to(root_url) unless current_user.admin?
 	end
   end
 
