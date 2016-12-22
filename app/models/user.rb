5a2a5dfce1f5e264d2a806760da206e9aa3996d6
DennisDaddy <softteez95@gmail.com>
2016-12-22 08:43:29 +0300

    Sending email via the user model object.

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,12 @@ class User < ApplicationRecord
 	update_attribute(:activated_at, Time.zone.now)
 	end
 
+	# Sends activation email.
+	def send_activation_email
+	 UserMailer.account_activation(self).deliver_now
+	end
+
+
 	#Returns true if the given token matches the digest.
     
 	def authenticated?(attribute, token)
