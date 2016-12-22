1df31fd81a1122e0619bc73c7177dc40c7bcca2e
DennisDaddy <softteez95@gmail.com>
2016-12-22 08:24:21 +0300

    add edit action

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -7,7 +7,6 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
-    assert_match CGI::escape(user.email), mail.body.encoded
   end
 
   test "password_reset" do
